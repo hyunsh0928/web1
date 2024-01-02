e6670f599f5f2cff447f4e74b56c4e7c468d0b8f
hyunsh0928 <155135084+hyunsh0928@users.noreply.github.com>
2024-01-02 11:25:18 +0900

    Add files via upload

---
+++ login_script.js
@@ -0,0 +1,16 @@
+    // 비밀번호 보이기/가리기 기능을 하는 함수입니다.
+    function togglePasswordVisibility() {
+        var passwordInput = document.getElementById('passwordInput');
+        var toggleImage = document.getElementById('toggleImage');
+  
+        if (passwordInput.type === 'password') {
+          passwordInput.type = 'text';
+          toggleImage.src = 'close.png'; // 보이기일 때 이미지 변경
+        } else {
+          passwordInput.type = 'password';
+          toggleImage.src = 'open.png'; // 가리기일 때 이미지 변경
+        }
+      }
+      document.querySelector('img').addEventListener('click', function() {
+        window.location.href = 'index.html'; // 여기에 주소 입력
+      });
